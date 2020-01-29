d937e6745be7d7685f539cb4df28bee64e13a2ce
bananhaj <bananohaj7@gmail.com>
2020-01-29 10:44:54 +0200

    test modified

--- test.js
+++ test.js
@@ -1,7 +1,19 @@
 var test = require('tape');
 var logic = require('./logic');
 
-test('Example test', function(t) {
-  t.pass();
-  t.end();
-});
+test('Refactor our add',function(t){
+t.equal(logic.addTodo(),'first task','this is the first task added');
+t.end();
+})
+
+test('Refactor our delete task',function(t){
+  t.equal(logic.deleteTodo(),'delete first task','this is the first deleted task');
+t.end();
+})
+
+test('Refactor our mark',function(t){
+t.equal(logic.markTodo(2),true,'mark task');
+
+t.end();
+})
+
